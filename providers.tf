18fd5cbb99fc37d67b6cf7d78be3eb4c3323b256
Ek C <ek.chote@gmail.com>
2021-09-30 10:54:33 +0700

    Use local_file instead of null_resource/local-exec provisioner to create template

--- providers.tf
+++ providers.tf
@@ -4,6 +4,10 @@ terraform {
       source = "digitalocean/digitalocean"
       version = "~> 2.0"
     }
+    local = {
+      source = "hashicorp/local"
+      version = "2.1.0"
+    }
     null = {
       source = "hashicorp/null"
       version = "~> 3.1"
